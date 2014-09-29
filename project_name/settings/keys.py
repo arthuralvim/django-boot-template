4769dfba955c359918f39f063f770bf57d63fd2d
Arthur Alvim <afmalvim@gmail.com>
2014-09-29 01:10:18 -0300

    close #11

--- project_name/settings/keys.py
+++ project_name/settings/keys.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from decouple import config
+
 # put your api keys, important keys, etc here.
+
+
+USE_LIB_EXAMPLE = config('USE_LIB_EXAMPLE', default=False, cast=bool)
+
+if USE_LIB_EXAMPLE:
+    LIB_KEY = config('LIB_KEY')
