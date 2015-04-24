200d3aa083ff51eb16a8cc95bddc7a0073413bda
Arthur Alvim <afmalvim@gmail.com>
2015-04-24 13:27:00 -0300

    Updating the example app.

--- project_name/apps/example/admin.py
+++ project_name/apps/example/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from .models import Basic
+
+
+admin.site.register(Basic)
--- project_name/apps/example/models.py
+++ project_name/apps/example/models.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+
+class Basic(models.Model):
+
+    text = models.TextField(verbose_name='text', blank=True, null=True)
+    number = models.IntegerField(verbose_name='text', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "basic"
+        verbose_name_plural = "basics"
